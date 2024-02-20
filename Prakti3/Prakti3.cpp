bb299abe1ee5fa91091e90f917f78c147e2cf9ff
balbes0 <ff347@mail.ru>
2024-02-20 14:02:11 +0300

    /

--- Prakti3/Prakti3.cpp
+++ Prakti3/Prakti3.cpp
@@ -1,35 +1,27 @@
 ﻿#include <iostream>
 using namespace std;
-const int ROWS = 3;
-const int COLS = 5;
-
-void inputMatrix(double matrix[ROWS][COLS]) {
-    cout << "Введите элементы массива построчно:\n";
-    for (int i = 0; i < ROWS; ++i) {
-        cout << "Строка " << i + 1 << ":\n";
-        for (int j = 0; j < COLS; ++j) {
-            cout << "Элемент " << j + 1 << ": ";
-            cin >> matrix[i][j];
-        }
-    }
-}
-
-double averageOfRow(double row[], int size) {
-    double sum = 0.0;
-    for (int i = 0; i < size; ++i) {
-        sum += row[i];
-    }
-    return sum / size;
-}
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    double matrix[ROWS][COLS];
-    inputMatrix(matrix);
+    const int ROWS = 3;
+    const int COLUMNS = 5;
+    double matrica[ROWS][COLUMNS];
+
+    cout << "Введите элементы матрицы " << ROWS << "x" << COLUMNS << endl;
+    for (int i = 0; i < ROWS; ++i) {
+        cout << "Строка " << (i + 1) << ":\n";
+        for (int j = 0; j < COLUMNS; ++j) {
+            cin >> matrica[i][j];
+        }
+    }
     cout << "Средние арифметические для каждой строки:\n";
     for (int i = 0; i < ROWS; ++i) {
-        double average = averageOfRow(matrix[i], COLS);
-        cout << "Строка " << i + 1 << ": " << average << endl;
+        double sum = 0;
+        for (int j = 0; j < COLUMNS; ++j) {
+            sum += matrica[i][j];
+        }
+        double average = sum / COLUMNS;
+        cout << "Строка " << (i + 1) << ": " << average << endl;
     }
     return 0;
 }
